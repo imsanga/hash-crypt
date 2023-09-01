2ac7018db59bbaed0fee2b1ee90fee56a57ad078
madrasmonkey05 <madrascoder05@gmail.com>
2023-09-01 23:36:08 +0530

    Search tab added

--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import { CoinItem } from "./CoinItem";
 import { CoinDetails } from "../routes/CoinDetails";
 import { Link } from "react-router-dom";
 import "./Coins.css";
 
 export const Coins = (props) => {
+    const [search, setSearch] = useState('');
+
+    const handleChange = e => {
+        setSearch(e.target.value);
+    };
+
+    const filteredCoins = props.coins.filter(coins =>
+        coins.name.toLowerCase().includes(search.toLowerCase())
+    );
+
     return (
         <div className="container">
+            <div className="coin-search">
+                <form>
+                    <input
+                        className="coin-input"
+                        type="text"
+                        onChange={handleChange}
+                        placeholder="Search"
+                    />
+                </form>
+            </div>
             <div>
                 <div className="heading">
                     <p className="navRank">#</p>
@@ -15,13 +36,13 @@ export const Coins = (props) => {
                         <p className="coin-priceChange hide-mobileC3">24h %</p>
                         <p className="coin-marketCap hide-mobileC2">Market cap</p>
                         <p className="coin-volume hide-mobileC1">Volume(24h)</p>
-                    </div>  
+                    </div>
                 </div>
 
-                {props.coins.map(coins => {
+                {filteredCoins.map(coins => {
                     return (
                         <Link to={`/coin/${coins.id}`} element={<CoinDetails />} key={coins.id}>
-                            <CoinItem coins={coins}/>
+                            <CoinItem coins={coins} />
                         </Link>
                     )
                 })}
--- src/components/NavbarCD.js
+++ src/components/NavbarCD.js
@@ -4,12 +4,14 @@ import "./Navbar.css"
 
 export const NavbarCD = () => {
     return (
-        <Link to="/">
-            <div className="navbar">
-                <FaCoins className="icon" />
-                <h1>Hash <span className="purple">Crypt</span></h1>
-            </div>
+        <div>
+            <Link to="/">
+                <div className="navbar">
+                    <FaCoins className="icon" />
+                    <h1>Hash <span className="purple">Crypt</span></h1>
+                </div>
+            </Link>
             <p className="slogan">Unlock the Future of Finance</p>
-        </Link>
+        </div>
     )
 }
